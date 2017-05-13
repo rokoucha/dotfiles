a081295921b7bb89d565930b0ebb70f8cdb751dd
Atnanasi <ys2000pro@gmail.com>
2017-05-13 15:53:59 +0900

    Fix prezto settings

--- install/post.sh
+++ install/post.sh
@@ -5,7 +5,6 @@ curl -sL zplug.sh/installer | zsh
 
 # For prezto
 ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zprezto
-ln -s $HOME/.zplug/repos/sorin-ionescu/prezto $HOME/.zpreztorc
 
 # I want to automated install vim-plug!!!
 echo "You need to exec this command in zsh 'zplug install'"
