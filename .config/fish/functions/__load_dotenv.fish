4b8b1d028c6f575d133dcb90bd4571303c977800
Rokoucha <git@rokoucha.net>
2020-09-07 13:07:31 +0900

    dotenv

---
+++ .config/fish/functions/__load_dotenv.fish
@@ -0,0 +1,8 @@
+function __load_dotenv --on-variable PWD
+    if test -r .env
+        for line in (cat .env)
+            set -l kv (string split -m 1 = -- $line)
+            set -x $kv
+        end
+    end
+end
