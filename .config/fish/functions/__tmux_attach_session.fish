892bf5ad3f55d9135e033c2681af96dd86f1a446
Rokoucha <git@rokoucha.net>
2020-09-07 12:40:32 +0900

    eval cmd

--- .config/fish/functions/__tmux_attach_session.fish
+++ .config/fish/functions/__tmux_attach_session.fish
@@ -7,7 +7,7 @@ function __tmux_attach_session
 
     set new_session "Create new session"
     set kill_sessions "Kill all sessions"
-    set ID (string join \n $new_session (string split \n $sessions) $kill_sessions | __fzfcmd | cut -d: -f1)
+    set ID (string join \n $new_session (string split \n $sessions) $kill_sessions | eval (__fzfcmd) | cut -d: -f1)
     if test "$ID" = "$new_session"
         exec tmux new-session
     else if test "$ID" = "$kill_sessions"
