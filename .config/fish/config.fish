21736c56057a48ef8b3e83d5435e6a50ab4a08af
Rokoucha <git@rokoucha.net>
2021-04-08 03:11:15 +0900

    feat: parse dotenv with comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,12 +11,17 @@ eval (dircolors -c ~/.dircolors)
 
 # Hooks
 function __load_dotenv --on-variable PWD
-    if test -r .env
-        for line in (cat .env)
-            set -l kv (string split -m 1 = -- $line)
-            set -gx $kv
-        end
+  if test -r .env
+    for line in (cat .env)
+      set -l matches (string match -r "^\s*(?:export\s+)?([\w\d_]+)=['\"]?(.*?)['\"]?(?:\s+#.+)?\$" "$line")
+
+      if test (count $matches) -ne 3
+        continue
+      end
+
+      set -gx $matches[2] $matches[3]
     end
+  end
 end
 
 # binding
