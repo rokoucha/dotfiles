dfde822fdda76d62ac0c5e7397eedc9db150ffff
Yuki Saito <rokoucha@hatena.ne.jp>
2025-09-19 15:21:07 +0900

    update

--- .config/fish/conf.d/50-android-tools.fish
+++ .config/fish/conf.d/50-android-tools.fish
@@ -1,3 +1,8 @@
 if [ -d /Users/rokoucha/Library/Android/sdk/platform-tools ]
     fish_add_path /Users/rokoucha/Library/Android/sdk/platform-tools
 end
+
+if [ -d /opt/homebrew/opt/opejdk/bin ]
+    fish_add_path /opt/homebrew/opt/openjdk/bin
+end
+
